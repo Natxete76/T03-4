5e3a199b63b748fa9f52f3b4cc13b88927f8ce63
Ignacio Garcia Vazquez <natxoga@gmail.com>
2022-06-21 08:16:20 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace T03_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int N = 8;
+            Console.WriteLine(N);
+            Console.WriteLine(N += 77);
+            Console.WriteLine(N -=3);
+            Console.WriteLine(N*=2);
+
+            Console.ReadKey();
+        }
+    }
+}
